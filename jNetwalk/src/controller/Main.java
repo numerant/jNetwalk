9423ca0f0560d068e6a524903e8a9dfd82e16924
Jakub Maleszewski <numerant@gmail.com>
2014-05-18 12:05:00 +0200

    Initial commit - create Main and MainWindow classes

---
+++ jNetwalk/src/controller/Main.java
@@ -0,0 +1,27 @@
+/**
+ * jNetwalk - simple netwalk game written in Java
+ * 
+ * @author Jakub Maleszewski
+ */
+
+package controller;
+
+/**
+ * @author Jakub Maleszewski
+ * @since 2014-05-18
+ * 
+ *
+ */
+public class Main
+{
+
+    /**
+     * Main application method
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
